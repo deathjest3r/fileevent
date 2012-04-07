8137be46d47fc22247e5e1cf9e64470b68ae2d4c
Death Jester <Death.Jester@web.de>
2012-04-07 22:53:32 +0200

    Revert "Shortest version possible, but has some flaws…"
    This reverts commit b5f42a9beda9ae3f97c2cbc90d59f9f102b02ac7.

--- lib/fileevent.rb
+++ lib/fileevent.rb
@@ -6,12 +6,12 @@ require 'statemachine'
 class Event
     attr_reader :type, :time, :path, :hash
     
-    def initialize(event)
-        if event.size == 4
-            @type = event[0]
-            @time = Integer(event[1])
-            @path = event[2]
-            @hash = event[3]
+    def initialize(*args)
+        if args.size == 1
+            @type = args[0][0]
+            @time = Integer(args[0][1])
+            @path = args[0][2]
+            @hash = args[0][3]
         end
     end
 end
@@ -20,29 +20,37 @@ class StateMachineContext
     attr_accessor :c_event, :l_event
     
     def initialize
-        @c_event = Event.new(['',0,'',''])
     end
     
     def add_object
-        obj = 'file'
-        obj = 'folder' if @c_event.hash == '-'
-        puts "Added #{obj} #{@c_event.path}"
+        if @c_event.hash == '-'
+            puts "Added folder #{@c_event.path}"
+        else
+            puts "Added file #{@c_event.path}"
+        end
     end
     
     def del_object
-        obj = 'file'
         if @l_event.type == 'DEL'
-            obj = 'folder' if @l_event.hash == '-'
-            puts "Deleted #{obj} #{@l_event.path}"
+            if @l_event.hash == '-'
+                puts "Deleted folder #{@l_event.path}"
+            else
+                puts "Deleted file #{@l_event.path}"
+            end
         end
-        obj = 'folder' if @c_event.hash == '-'
-        puts "Deleted #{obj} #{@c_event.path}"
+        if @c_event.hash == '-'
+            puts "Deleted folder #{@c_event.path}"
+        else
+            puts "Deleted file #{@c_event.path}"
+        end
     end
     
     def move_object
-        obj = 'file'
-        obj = 'folder 'if @c_event.hash == '-' && @l_event.hash == '-'
-        puts "Moved #{obj} #{@l_event.path} -> #{@c_event.path}"
+        if @c_event.hash == '-' && @l_event.hash == '-'
+            puts "Moved folder #{@l_event.path} -> #{@c_event.path}"
+        else
+            puts "Renamed file #{@l_event.path} -> #{@c_event.path}"
+        end
     end
 end
 
@@ -52,27 +60,43 @@ class FileEvent
     end
         
     def run
-        sm = statemachine
-        
         begin
             events = Integer(gets())
         rescue
+            puts 'Unknown event count'
             retry
         end
         
         while(events > 0)
             event = Event.new(gets().split(' '))
-            
-            sm.context.l_event = sm.context.c_event 
-            sm.context.c_event = event
-            
-            if sm.context.c_event.time > sm.context.l_event.time
-                sm.add if event.type == 'ADD'
-                sm.del if event.type == 'DEL'
+
+            if @events[event.time] == nil
+                @events[event.time] = [event] 
+            else
+                @events[event.time] << event
             end
             events-=1
         end
         
+        exec_events
+    end
+        
+    def exec_events
+        sm = statemachine
+        
+        times = @events.keys.sort
+        times.each do |time|
+            sm.context.l_event = sm.context.c_event 
+            sm.context.c_event = @events[time][0]
+
+            if @events[time][0].type == 'ADD'
+                sm.add
+            elsif @events[time][0].type == 'DEL'
+                sm.del
+            end
+        end
+        
+        # When final event is del, handle it...
         if sm.state == :stage1
             sm.context.del_object
         end
@@ -94,11 +118,17 @@ class FileEvent
                 event :del, :stage0, :del_object
                 on_entry :move_object
             end
+            
+            
             context StateMachineContext.new
         end
         return sm
     end
 end
 
-fe = FileEvent.new
-fe.run
+def main
+    fe = FileEvent.new
+    fe.run
+end
+
+main
