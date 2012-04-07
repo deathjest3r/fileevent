8459920c8266db2b90a8a05badb054d27dea587e
Death Jester <Death.Jester@web.de>
2012-04-07 22:29:30 +0200

    Newest version of the file event challenge, finally decided to use a Ruby gem for help

--- lib/fileevent.rb
+++ lib/fileevent.rb
@@ -1,106 +1,64 @@
 #!/usr/bin/env ruby
 
+require 'rubygems'
+require 'statemachine'
+
 class Event
     attr_reader :type, :time, :path, :hash
     
     def initialize(*args)
         if args.size == 1
             @type = args[0][0]
-            @time = args[0][1]
+            @time = Integer(args[0][1])
             @path = args[0][2]
             @hash = args[0][3]
         end
     end
 end
 
-class EventFolder
-    attr_reader :path
-    attr_accessor :files
+class StateMachineContext
+    attr_accessor :c_event, :l_event
     
-    def initialize(event)
-        @path = event.path
-        @files = Hash.new
-        @events = [event]
+    def initialize
     end
     
-    # Events noch zeitlich sortieren
-    def add_event(event)
-        @events << event
+    def add_object
+        if @c_event.hash == '-'
+            puts "Added folder #{@c_event.path}"
+        else
+            puts "Added file #{@c_event.path}"
+        end
     end
     
-    def is_empty
-        return true if @files.empty?
-        return false
-    end
-end
-
-class EventFile
-    attr_reader :hash, :parent
-    
-    def initialize(event)
-        # Exception werfen falls split fehlschlaegt
-        @hash = event.hash
-        @parent = File.dirname(event.path)
-        @events = [event]
-    end
-    
-    # Events noch zeitlich sortieren
-    def add_event(event)
-        @events << event
-    end
-    
-    def handle_events
-        @events.each_with_index do |evt, idx|
-            # Jump to next event if event is nil
-            next if evt == nil
-            # Check if first event is an ADD event, otherwise discard
-            @events[idx] = nil if idx == 0 && evt.type == 'DEL'
-            # Send msg, for creation of an element
-            puts "Added file #{evt.path}." if idx == 0 && evt.type == 'ADD'
-            
-            
-            if idx > 0 && @events[idx-1].type == 'DEL' && evt.type == 'ADD'
-                puts "File moved #{@events[idx-1].path} -> #{evt.path}."
+    def del_object
+        if @l_event.type == 'DEL'
+            if @l_event.hash == '-'
+                puts "Deleted folder #{@l_event.path}"
+            else
+                puts "Deleted file #{@l_event.path}"
             end
-            
-            if idx > 0 && @events[idx-1].type == 'ADD' && evt.type == 'ADD'
-                puts "File copied #{@events[idx-1].path} -> #{evt.path}."
-            end  
+        end
+        if @c_event.hash == '-'
+            puts "Deleted folder #{@c_event.path}"
+        else
+            puts "Deleted file #{@c_event.path}"
+        end
+    end
+    
+    def move_object
+        if @c_event.hash == '-' && @l_event.hash == '-'
+            puts "Moved folder #{@l_event.path} -> #{@c_event.path}"
+        else
+            puts "Renamed file #{@l_event.path} -> #{@c_event.path}"
         end
     end
 end
 
 class FileEvent
     def initialize
-        @folders = Hash.new
+        @events = Hash.new
     end
         
-    def parse_events(e)
-        event = Event.new(e.split(' '))
-        
-        if event.hash == '-'
-            folder = EventFolder.new(event)
-            
-            if @folders[folder.path] == nil
-                @folders[folder.path] = folder
-            else
-                @folders[folder.path].add_event(event)
-            end
-        else
-            file = EventFile.new(event)
-            
-            # Created file without folder...
-            return if @folders[file.parent] == nil
-            
-      
-            if @folders[file.parent].files[file.hash] == nil
-                @folders[file.parent].files[file.hash] = file
-            else
-                @folders[file.parent].files[file.hash].add_event(event)
-            end
-        end  
-    end
-    
     def run
         begin
             events = Integer(gets())
@@ -110,14 +68,61 @@ class FileEvent
         end
         
         while(events > 0)
-            parse_events(gets())
+            event = Event.new(gets().split(' '))
+
+            if @events[event.time] == nil
+                @events[event.time] = [event] 
+            else
+                @events[event.time] << event
+            end
             events-=1
         end
         
-#        @folders.each do |hash, file|
-#            #puts hash
-#            file.handle_events
-#        end
+        exec_events
+    end
+        
+    def exec_events
+        sm = statemachine
+        
+        times = @events.keys.sort
+        times.each do |time|
+            sm.context.l_event = sm.context.c_event 
+            sm.context.c_event = @events[time][0]
+
+            if @events[time][0].type == 'ADD'
+                sm.add
+            elsif @events[time][0].type == 'DEL'
+                sm.del
+            end
+        end
+        
+        # When final event is del, handle it...
+        if sm.state == :stage1
+            sm.context.del_object
+        end
+    end
+    
+    def statemachine
+        sm = Statemachine.build do
+            state :stage0 do
+                event :del, :stage1
+                event :add, :stage0, :add_object 
+            end
+            state :stage1 do
+                event :add, :stage2
+                event :del, :stage0, :del_object
+                
+            end
+            state :stage2 do
+                event :add, :stage0, :add_object
+                event :del, :stage0, :del_object
+                on_entry :move_object
+            end
+            
+            
+            context StateMachineContext.new
+        end
+        return sm
     end
 end
 
